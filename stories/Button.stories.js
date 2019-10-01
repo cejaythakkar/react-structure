f5626d8fabde83633ffd6b5b03b48705264fb495
cejaythakkar <ce.jaythakkar@gmail.com>
2019-10-01 20:20:06 +0530

    asd

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -4,5 +4,5 @@ import { storiesOf } from '@storybook/react';
 import MyButton from '../src/app/components/Button/Button';
 
 storiesOf('Button1', module)
-.addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
-.add('default', () => <MyButton label={`Try 30 days free trial`} />);
+  .addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
+  .add('default', () => <MyButton label={`Try 30 days free trial`} />);
